e4b9f34cc55141cb2cdc3cea89204660b519c428
PinoLopez <creadoresdigitales@gmail.com>
2026-03-10 14:59:33 +0100

    modified:   .github/workflows/playwright.yml 	modified:   playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './suites',
-
+  testDir: './',
   fullyParallel: true,
-
   forbidOnly: !!process.env.CI,
-
   retries: process.env.CI ? 2 : 0,
-
   workers: process.env.CI ? 1 : undefined,
 
   reporter: [
@@ -17,18 +13,14 @@ export default defineConfig({
 
   use: {
     baseURL: 'https://en.wikipedia.org',
-
     trace: 'on-first-retry',
-
     screenshot: 'only-on-failure',
-
-    video: 'retain-on-failure'
   },
 
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] }
-    }
-  ]
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
 });
